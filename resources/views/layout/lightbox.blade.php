584cf924f3de1a2af62e591b6ef6f03693fe099b
Stef Kariotidis <stef.kariotidis@gmail.com>
2017-01-25 21:04:59 +0200

    robots.txt disallow admin path

--- resources/views/layout/lightbox.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="modal">
-    <div class="modal-background"></div>
-    <div class="modal-content">
-        <p class="image">
-        </p>
-    </div>
-    <button class="modal-close"></button>
-</div>
